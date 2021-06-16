97e40bfb3550b4d9552a8a2eec7471f7c8a1b7d5
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-06-15 22:06:05 -0300

    fix: button list return

--- pages/components/Button/ButtonList.tsx
+++ pages/components/Button/ButtonList.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import { Button, ButtonProps } from './Button';
 
 type ButtonListProps = {
@@ -5,5 +6,11 @@ type ButtonListProps = {
 };
 
 export const ButtonList: React.FC<ButtonListProps> = ({ items }) => {
-  return items.map((item, index) => <Button key={index} {...item} />);
+  return (
+    <Fragment>
+      {items.map((item, index) => (
+        <Button key={index} {...item} />
+      ))}
+    </Fragment>
+  );
 };
