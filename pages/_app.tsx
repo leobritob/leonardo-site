49fdcf373046b9cb59435bb7e0018af37bc0d5c0
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-06-18 11:13:37 -0300

    fix: facebook pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -33,11 +33,11 @@ function MyApp({ Component, pageProps }) {
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '307877267656838');
+          fbq('init', '554067208928075');
           fbq('track', 'PageView');
           </script>
           <noscript><img height="1" width="1" style="display:none"
-          src="https://www.facebook.com/tr?id=307877267656838&ev=PageView&noscript=1"
+          src="https://www.facebook.com/tr?id=554067208928075&ev=PageView&noscript=1"
           /></noscript>
           <!-- End Facebook Pixel Code -->
         `,
